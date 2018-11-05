d8242996ec6ca95a548cd14234ec5e60a56d9bde
chenshuai <384619043@qq.com>
2018-11-05 21:55:42 +0800

    one

---
+++ components/public/header/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-row
+    :gutter="15"
+    class="m-header">
+    <el-col :span="6">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      1
+    </el-col>
+    <el-col :span="6">
+      2
+    </el-col>
+    <el-col :span="6">
+      3
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from './geo'
+    export default {
+        name: "Index",
+      components: {
+          Geo
+      }
+    }
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss";
+</style>
